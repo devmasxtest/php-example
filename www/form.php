f034230e8ef20230b1aa17c7e4e5323c0ec289c1
Miguel Savignano <migue.masx@gmail.com>
2023-06-18 15:59:18 +0200

    simple form

---
+++ www/form.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+include("./funcionesArrays.php");
+$FIELD_NAME = "usuarioNombre";
+?>
+<html>
+
+<head>
+    <title>Formulario</title>
+</head>
+
+<body>
+    <?php
+    if (!isset($_SESSION[$FIELD_NAME])) {
+        $_SESSION[$FIELD_NAME] = "";
+    } else {
+        if (!empty($_POST)) {
+            $nombre = filter_input(INPUT_POST, $FIELD_NAME, FILTER_SANITIZE_STRING);
+            $_SESSION[$FIELD_NAME] = $nombre;
+        } else {
+            echo "Datos incorrectamentos";
+        }
+    }
+    ?>
+
+    <h1>
+        Hola:
+        <?= $_SESSION[$FIELD_NAME] ?>
+    </h1>
+
+    <form method="POST">
+        <label for="nombre"> ¿Cual es tu nombre?: </label>
+        <input type="text" name="<?= $FIELD_NAME ?>">
+        <br>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+
+</html>
